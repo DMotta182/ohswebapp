ee614efc4351ee1d53915fd5c65e8404d931f122
Diego Motta <diegomotta@royalestudios.com>
2020-06-14 12:22:30 -0600

    Updates

---
+++ precache-manifest.2cc13143f6e6e5de925d9dc1010d17e3.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b9e0690b27042485365066c030838ed",
+    "url": "/ohswebapp/index.html"
+  },
+  {
+    "revision": "55ef40e8564659216788",
+    "url": "/ohswebapp/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "2b1481cbd59ce2a1fdd3",
+    "url": "/ohswebapp/static/css/main.ddcd8762.chunk.css"
+  },
+  {
+    "revision": "55ef40e8564659216788",
+    "url": "/ohswebapp/static/js/2.4389cff0.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/ohswebapp/static/js/2.4389cff0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2b1481cbd59ce2a1fdd3",
+    "url": "/ohswebapp/static/js/main.3262fab0.chunk.js"
+  },
+  {
+    "revision": "926b9af73dec5b466350",
+    "url": "/ohswebapp/static/js/runtime-main.aa3a409d.js"
+  },
+  {
+    "revision": "4a7a504dcee8366f3336806af50784c1",
+    "url": "/ohswebapp/static/media/check.4a7a504d.svg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ohswebapp/static/media/logo.5d5d9eef.svg"
+  }
+]);
