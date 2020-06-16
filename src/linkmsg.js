ac66f87b14ab77019f5f16dad10a6be9e2d004f2
Diego Motta <diegomotta@royalestudios.com>
2020-06-15 20:15:29 -0600

    stable

--- src/linkmsg.js
+++ src/linkmsg.js
@@ -1,5 +1,6 @@
 export const linkRec = () => {
   const location = 'localhost:3000'
+  //const location = 'https://dmotta182.github.io/ohswebapp'
   const msg = 'Comparte este link para ganar\r\n' + location + '/item/show'; 
   alert(msg)
 }
